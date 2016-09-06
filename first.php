fd38dd00e5f412244a0881af092926a6090f11a3
karlbork <karlbork@greeny.cs.tlu.ee>
2016-09-06 15:16:44 +0300

    added my first php file

---
+++ first.php
@@ -0,0 +1,16 @@
+<?php
+
+	$firstName = "Karl-Erik";
+	$lastName = "Borkmann";
+
+	//echo trükib muutuja veebilehel välja
+	
+	//echo $firstName;
+	//echo " ";
+	//echo $lastName;
+	
+	//punkt liidab stringe
+	
+	echo $firstName." ".$lastName
+	
+?>
